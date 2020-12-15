7fd294b015b659ef19518815ee826769b7240079
Alex <alexdolzenko@gmail.com>
2020-12-15 22:31:58 +0000

    Отправка сообщений в чат

---
+++ src/lesson4/ServerGUI.java
@@ -0,0 +1,69 @@
+package lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ServerGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+
+    private static final int POS_X = 800;
+    private static final int POS_Y = 400;
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 150;
+
+    private final ChatServer chatServer = new ChatServer();
+    private final JButton btnStart = new JButton("Start");
+    private final JButton btnStop = new JButton("Stop");
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() { //Event dispatching thread
+            @Override
+            public void run() {
+                new ServerGUI(); //Очередь событий
+            }
+        });
+        throw new RuntimeException("Hello from main");
+    }
+
+    private ServerGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat Server");
+        setAlwaysOnTop(true);
+        setLayout(new GridLayout(1, 2));
+
+        btnStart.addActionListener(this);
+        btnStop.addActionListener(this);
+
+        add(btnStart);
+        add(btnStop);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == btnStart) {
+            chatServer.start(11111);
+        } else if (src == btnStop) {
+            throw new RuntimeException("Hello from EDT!");
+            //chatServer.stop();
+        } else {
+            throw new RuntimeException("Undefined source: " + src);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        StackTraceElement[] ste = e.getStackTrace();
+        String msg = String.format("Exception in thread \"%s\" %s: %s\n\tat %s",
+                t.getName(), e.getClass().getCanonicalName(),
+                e.getMessage(), ste[0]);
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+}
